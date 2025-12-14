8fcde3ab9f9274e4f62d87a0e786f46343a18f2f
Ambushfall <lukathetormentor@gmail.com>
2025-12-14 17:22:54 +0100

    added porezko, removed popups, added firefox

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,20 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from 'wxt'
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
-});
+  manifest: {
+    host_permissions: [
+      // 'https://lpa.gov.rs/jisportal/wus/obveznik/*/wusdatalist',
+      'https://nbs.rs/QRcode/api/qr/v1/gen/500'
+    ],
+    browser_specific_settings:{
+      gecko: {
+        // @ts-ignore - WXT doesn't support this field yet
+        data_collection_permissions: {
+          required: ['none'],
+        },
+      }
+    }
+  }
+})
